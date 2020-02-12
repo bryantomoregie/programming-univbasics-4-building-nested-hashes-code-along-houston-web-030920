1a66289e666d83eea314d324abcca93716e5b8ad
Bryant Omoregie <unclebryant@gmail.com>
2020-02-12 20:24:16 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -12,8 +12,8 @@ def third_challenge
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"
         },
-      :matriarch => {
-        
+      :matriarch => {name: "Lady Capulet", age: "51"
+      },
       :heroine => {
         
         },
