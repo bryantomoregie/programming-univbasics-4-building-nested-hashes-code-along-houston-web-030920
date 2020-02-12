372fdb0827a4b647364f48c754de0550d1a0c9cd
Bryant Omoregie <unclebryant@gmail.com>
2020-02-12 20:23:01 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -3,8 +3,7 @@ def third_challenge
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"
         },
-      :matriarch => {
-        
+      :matriarch => {name: "Lady Montague", age: "54"
         },
       :hero => {
         
