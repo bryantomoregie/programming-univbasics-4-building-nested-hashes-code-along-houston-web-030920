7fc67963b3b1a33f1fd8578266cc646ceedfbe7c
Bryant Omoregie <unclebryant@gmail.com>
2020-02-12 20:26:43 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -14,9 +14,8 @@ def third_challenge
         },
       :matriarch => {name: "Lady Capulet", age: "51"
       },
-      :heroine => {
-        
-        },
+      :heroine => {name: "Juliet", age: "15", status: "alive"
+       },
       :heroine_friends => []
    }
   }
