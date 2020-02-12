d2610e29b29a58c42804174311213b8c3e1b1bca
Bryant Omoregie <unclebryant@gmail.com>
2020-02-12 20:38:24 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,8 +21,8 @@ def bonus
   }
 
  
-  epic_tragedy[:montague][:patriarch][:hero][:status] = "dead"
-  
+  epic_tragedy[:montague][:hero][:status] = "dead"
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
 
   
 
